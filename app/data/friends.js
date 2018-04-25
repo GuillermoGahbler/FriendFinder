30a0949a6b34ac07d75314cbbdbd5a26aaf5af77
GuillermoGahbler <mexamericano@gmail.com>
2018-04-25 14:14:20 -0700

    updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,23 +3,70 @@ module.exports = {
             "name": "Billy",
             "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
             "scores": [
-                5, 1, 4, 4, 5, 1, 2, 5, 4, 1
+                10,10,10,10,10,10,10,10,1,1
             ]
         },
         {
             "name": "Ace",
-            "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+            "photo": "https://i0.wp.com/www.thatmomentin.com/wp-content/uploads/2017/01/jim.jpg?resize=670%2C430",
             "scores": [
-                2, 2, 2, 2, 2, 2, 2, 2, 2, 2
+                2,2,2,2,2,2,2,2,2,2
             ]
         },
         {
             "name": "Bruiser",
+            "photo": "https://prowrestlingstories.com/wp-content/uploads/2017/07/Screenshot-2017-07-16-at-11.47.03-PM-e1500266889597.png",
+            "scores": [
+                3,3,3,3,3,3,3,3,3,3            ]
+        },
+        {
+            "name": "Cloud",
             "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
             "scores": [
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+                4,4,4,4,4,4,4,4,4,4
             ]
         },
-        //   {},{},{},{},{},{},{},{},{},{}
+        {
+            "name": "Frodo",
+            "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+            "scores": [
+                5,5,5,5,5,5,5,5,5,5
+            ]
+        },
+        {
+            "name": "Ash",
+            "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+            "scores": [
+                6,6,6,6,6,6,6,6,6,6
+            ]
+        },
+        {
+            "name": "Duke",
+            "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+            "scores": [
+                7,7,7,7,7,7,7,7,7,7
+            ]
+        },
+        {
+            "name": "Barry Badernath",
+            "photo": "https://static1.squarespace.com/static/54b854c9e4b089bfadc9b5e2/54d3c195e4b028e7b23f3d18/54d3c1c8e4b04682afead9f9/1423163854573/06---Who-is-Barry-Badrinath-.jpg",
+            "scores": [
+                8,8,8,8,8,8,8,8,8,8
+            ]
+        },
+        {
+            "name": "Harry",
+            "photo": "https://nerdist.com/wp-content/uploads/2017/01/harry-potter.jpg",
+            "scores": [
+                9,9,9,9,9,9,9,9,9,9
+            ]
+        },
+        {
+            "name": "Joffrey",
+            "photo": "https://imgix.bustle.com/elite-daily/2017/07/28103007/joffrey-game-of-thrones-choking.jpg?w=998&h=598&fit=crop&crop=faces&auto=format&q=70",
+            "scores": [
+                10,10,10,10,10,10,10,10,10,10
+            ]
+        }
     ]
 }
